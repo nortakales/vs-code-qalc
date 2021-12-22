94028bf582736a69380439707efdae1ba458c026
Nick Ortakales <nortakales@gmail.com>
2021-12-21 18:34:43 -0800

    Changed currency API call to not use API key

--- src/currency.ts
+++ src/currency.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ExtensionContext } from 'vscode';
 
 // 7 days
 const dataTtl = 7 * 24 * 60 * 60 * 1000;
@@ -11,7 +12,6 @@ export interface ExchangeData {
     },
 }
 
-//@ts-ignore
 export async function getExchangeRates(ctx: ExtensionContext): Promise<ExchangeData> {
     let data = ctx.globalState.get<ExchangeData>("exchangeRates");
 
@@ -19,9 +19,7 @@ export async function getExchangeRates(ctx: ExtensionContext): Promise<ExchangeD
         console.log("Fetching latest currency exchange rates...");
 
         try {
-            // Temporarily non-https and free access key until finding a new solution
-            // Looks like https://api.exchangerate.host/latest?base=USD might work
-            let response = await axios.get("http://api.exchangeratesapi.io/latest?access_key=ce3783aaf8f32b673a37a1d73479a80c");
+            let response = await axios.get("https://api.exchangerate.host/latest?base=USD");
             data = response.data;
             await ctx.globalState.update("exchangeRates", data);
         } catch (error) {
@@ -31,6 +29,6 @@ export async function getExchangeRates(ctx: ExtensionContext): Promise<ExchangeD
 
     return data ?? {
         base: "USD",
-        rates: {USD: 1.0},
+        rates: { USD: 1.0 },
     };
 }
